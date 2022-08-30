f809cbd752bcdd0eed2dec19276e6c1272f3c8c4
Dana Huffman <dana.huffman@yahoo.com>
2022-08-30 15:29:06 -0700

    project fix

--- shelter_project/settings.py
+++ shelter_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1fo(^24hxjtkj5!novw)tik(j5%cheixp8i8r311bbeg%e^lfe'
+SECRET_KEY = 'django-insecure-2e==q9^pvt=q(u$3tnr$xizr+mysc*j!v&yho!w%2c0t)iuc8z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
